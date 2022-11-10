0c6bedac12b92011f42b6afeb2855525bd52de65
Gabriel Darbaiseli <50719588+Gabrora@users.noreply.github.com>
2022-11-10 21:34:25 +0400

    Update script.bash

--- script.bash
+++ script.bash
@@ -1,8 +1,12 @@
-
 #!/bin/bash
+
+# mostly copied from: https://medium.com/@iraklis/running-hashcat-v4-0-0-in-amazons-aws-new-p3-16xlarge-instance-e8fab4541e9b
+#
 sudo apt-get update -yq
 sudo apt-get install -yq build-essential linux-headers-$(uname -r) unzip p7zip-full linux-image-extra-virtual
-sudo apt-get install -yq python3-pip
+sudo apt-get install -yq ocl-icd-libopencl1 opencl-headers clinfo
+#sudo apt-get install -yq libhwloc-plugins libhwloc5 libltdl7 libpciaccess0 libpocl2 libpocl2-common ocl-icd-libopencl1 pocl-opencl-icd
+sudo apt-get install -yq python3-pip 
 pip3 install psutil
 
 sudo touch /etc/modprobe.d/blacklist-nouveau.conf
@@ -16,3 +20,7 @@ sudo touch /etc/modprobe.d/nouveau-kms.conf
 sudo bash -c "echo 'options nouveau modeset=0' >>  /etc/modprobe.d/nouveau-kms.conf"
 sudo update-initramfs -u
 sudo reboot
+
+### Install nVidia Drivers
+wget http://us.download.nvidia.com/tesla/410.104/NVIDIA-Linux-x86_64-410.104.run
+sudo /bin/bash NVIDIA-Linux-x86_64-410.104.run --ui=none --no-questions --silent -X
